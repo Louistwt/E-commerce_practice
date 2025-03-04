21407281bf146621194f13c5f9f506e3f37ff3f4
Louis_twt <louistse5283@gmail.com>
2025-03-04 12:05:18 +0000

    completed cart controller

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,3 +1,19 @@
+import Product from "../models/product.model.js"
+
+export const getCartProducts = async (req, res) => {
+    try {
+        const products = await Product.find({_id: {$in: req.user.cartItems}})
+
+        const cartItems = products.map(products => {
+            const item = req.user.cartItems.find(cartItem => cartItem.id === products.id);
+            return { ...products.toJSON(), quantity: item.quantity }
+        });
+    } catch (error) {
+        console.log("Error in getCartProducts controller", error.message)
+        res.status(500).json({ message: "Server error", error: error.message })        
+    }
+}
+
 export const addToCart = async (req, res) => {
     try {
         const {productId} = req.body;
@@ -33,5 +49,31 @@ export const removeAllFromCart = async (req, res) => {
         res.json(user.cartItems)
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export const updateQuantity = async (req, res) => {
+    try {
+        const {id: productId} = req.params;
+        const {quantity} = req.body;
+        const user = req.user;
+        const existingItem = user.cartItems.find((item) => item.id === productId)
+
+        if (existingItem) {
+            if (quantity === 0) {
+                user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+                await user.save()
+                return res.json(user.cartItems)
+            }
+
+            existingItem.quantity = quantity;
+            await user.save();
+            res.json(user.cartItems);
+        } else {
+            res.status(404).json({ message: "Product not found" })
+        }
+    } catch (error) {
+        console.log("Error in updatedQuantity controller", error.message)
+        res.status(500).json({ message: "Server error", error: error.message })
     }
 }
