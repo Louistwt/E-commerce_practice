b4f0979be2c648fa6ede3a72d85e2ae9be29d085
Louis_twt <louistse5283@gmail.com>
2025-03-11 21:51:28 +0000

    set up signup, login, logout and axios

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from 'lucide-react';
 import { motion } from 'framer-motion';
+import { useUserStore } from '../stores/useUserStore.js';
 
 const SignUpPage = () => {
-  const loading = false;
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -12,9 +12,11 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
+  const { signup, loading } = useUserStore()
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    signup(formData)
   }
 
   return (
@@ -52,7 +54,7 @@ const SignUpPage = () => {
 									onChange={(e) => setFormData({ ...formData, name: e.target.value })}
 									className='block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm
 									 placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
-									placeholder='example001'
+									placeholder='Enter username'
 								/>
 							</div>
 						</div>
@@ -96,7 +98,7 @@ const SignUpPage = () => {
 									onChange={(e) => setFormData({ ...formData, password: e.target.value })}
 									className=' block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 
 									rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
-									placeholder='your password'
+									placeholder='Enter password'
 								/>
 							</div>
 						</div>
@@ -117,7 +119,7 @@ const SignUpPage = () => {
 									onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
 									className=' block w-full px-3 py-2 pl-10 bg-gray-700 border
 									 border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
-									placeholder='confirm password'
+									placeholder='Confirm password'
 								/>
 							</div>
 						</div>
